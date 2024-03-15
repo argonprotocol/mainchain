f2b98f793eafaa806ce565c76aeaabd4bebb9936
Blake Byrnes <blakebyrnes@gmail.com>
2024-03-15 14:40:53 -0400

    chore: update scripts

---
+++ scripts/create_chain_spec.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# This script is meant to be run on Unix/Linux based systems
+set -e
+
+./target/release/ulx-node build-spec
--- scripts/test_network.sh
+++ scripts/test_network.sh
@@ -3,7 +3,7 @@
 set -e
 
 BASEDIR=$(dirname "$0")/..
-
+DBPATH=postgres://postgres:password@localhost:5432/notary
 # create array of validators
 validators=(alice bob dave)
 
@@ -15,5 +15,14 @@ for i in {0..2} ; do
   echo "(\"$BASEDIR/target/release/ulx-node\" --tmp --${validators[$i]} --chain local --rpc-port=994$((i+4))  --port 3033$((i+4)) --miners 1 &)"
   "$BASEDIR/target/release/ulx-node" --tmp --${validators[$i]} --chain local --rpc-port=994$((i+4))  --port 3033$((i+4)) --miners 1 &
 done
+
+echo "(\"$BASEDIR/target/release/ulx-notary\" insert-key --keystore-path /tmp/notary_keystore --suri //Ferdie//notary)"
+"$BASEDIR/target/release/ulx-notary" insert-key --keystore-path /tmp/notary_keystore --suri //Ferdie//notary;
+
+echo "(\"$BASEDIR/target/release/ulx-node\" migrate --db-path ${DBPATH})"
+"$BASEDIR/target/release/ulx-notary" migrate --db-path ${DBPATH};
+
+echo "(\"$BASEDIR/target/release/ulx-node\" run --db-path ${DBPATH} -t ws://localhost:9944 --keystore-path /tmp/notary_keystore -b 127.0.0,1:9925)"
+"$BASEDIR/target/release/ulx-notary" run --db-path ${DBPATH} -t ws://localhost:9944 --keystore-path /tmp/notary_keystore -b "127.0.0,1:9925" &
 
 wait
