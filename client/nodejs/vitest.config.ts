7ed9223d7815dcc7c833e85e67e960931515773a
Blake Byrnes <blakebyrnes@gmail.com>
2025-04-22 11:32:51 -0400

    chore: format test output

--- client/nodejs/vitest.config.ts
+++ client/nodejs/vitest.config.ts
@@ -4,5 +4,12 @@ export default defineConfig({
   test: {
     testTimeout: 120_000,
     hookTimeout: 120_000,
+    retry: 1,
+    reporters: process.env.GITHUB_ACTIONS
+      ? ['dot', 'github-actions', 'junit']
+      : ['dot'],
+    outputFile: {
+      junit: 'vitest-results.xml',
+    },
   },
 });
--- localchain/vitest.config.ts
+++ localchain/vitest.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
     hookTimeout: 120_000,
     retry: 2,
     disableConsoleIntercept: true,
+    reporters: process.env.GITHUB_ACTIONS
+      ? ['dot', 'github-actions', 'junit']
+      : ['dot'],
+    outputFile: {
+      junit: 'vitest-results.xml',
+    },
   },
 });
