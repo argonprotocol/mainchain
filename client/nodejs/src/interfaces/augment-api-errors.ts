512deeff75f08c0fa75137299d2559ae4537de24
Blake Byrnes <blakebyrnes@gmail.com>
2026-02-05 17:13:43 -0500

    chore: upgrade to 1.88.0

--- client/nodejs/src/interfaces/augment-api-errors.ts
+++ client/nodejs/src/interfaces/augment-api-errors.ts
@@ -280,7 +280,8 @@ declare module '@polkadot/api-base/types/errors' {
        **/
       UtxoNotLocked: AugmentedError<ApiType>;
     };
-    blockRewards: {};
+    blockRewards: {
+    };
     blockSeal: {
       /**
        * A block seal authority could not be properly decoded
@@ -510,7 +511,8 @@ declare module '@polkadot/api-base/types/errors' {
        **/
       TooSoon: AugmentedError<ApiType>;
     };
-    hyperbridge: {};
+    hyperbridge: {
+    };
     ismp: {
       /**
        * Couldn't update challenge period
@@ -891,7 +893,8 @@ declare module '@polkadot/api-base/types/errors' {
        **/
       Unauthorized: AugmentedError<ApiType>;
     };
-    ticks: {};
+    ticks: {
+    };
     tokenGateway: {
       /**
        * Asset Id creation failed
