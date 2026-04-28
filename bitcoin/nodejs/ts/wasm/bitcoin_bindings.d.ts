d29ec9ef318b9c16d4c4ae6a3bdaff724b8a5d9a
Blake Byrnes <blakebyrnes@gmail.com>
2026-04-28 11:53:01 -0400

    build(bitcoin): replace wasm-pack with local bindgen build

--- bitcoin/nodejs/ts/wasm/bitcoin_bindings.d.ts
+++ bitcoin/nodejs/ts/wasm/bitcoin_bindings.d.ts
@@ -1,57 +1,31 @@
+/* tslint:disable */
+/* eslint-disable */
+
 export enum BitcoinNetwork {
-  Bitcoin,
-  Testnet,
-  Regtest,
-  Signet,
+    /**
+     * Mainnet Bitcoin.
+     */
+    Bitcoin = 0,
+    /**
+     * Bitcoin's testnet network.
+     */
+    Testnet = 1,
+    /**
+     * Bitcoin's signet network
+     */
+    Signet = 2,
+    /**
+     * Bitcoin's regtest network.
+     */
+    Regtest = 3,
 }
 
-export function createCosignPubkey(
-  vaultPubkeyHex: string,
-  vaultClaimPubkeyHex: string,
-  ownerPubkeyHex: string,
-  vaultClaimHeight: bigint | number,
-  openClaimHeight: bigint | number,
-  createdAtHeight: bigint | number,
-  bitcoinNetwork: BitcoinNetwork,
-): string;
+export function calculateFee(vault_pubkey_hex: string, vault_claim_pubkey_hex: string, owner_pubkey_hex: string, vault_claim_height: bigint, open_claim_height: bigint, created_at_height: bigint, bitcoin_network: BitcoinNetwork, fee_rate_sats_per_vb: bigint, to_script_pubkey: string): bigint;
 
-export function calculateFee(
-  vaultPubkeyHex: string,
-  vaultClaimPubkeyHex: string,
-  ownerPubkeyHex: string,
-  vaultClaimHeight: bigint | number,
-  openClaimHeight: bigint | number,
-  createdAtHeight: bigint | number,
-  bitcoinNetwork: BitcoinNetwork,
-  feeRateSatsPerVb: bigint | number,
-  toScriptPubkey: string,
-): bigint;
+export function createCosignPubkey(vault_pubkey_hex: string, vault_claim_pubkey_hex: string, owner_pubkey_hex: string, vault_claim_height: bigint, open_claim_height: bigint, created_at_height: bigint, bitcoin_network: BitcoinNetwork): string;
 
-export function signPsbtDerived(
-  psbtHex: string,
-  xprivB58: string,
-  xprivHdPath: string,
-  finalize: boolean,
-): string;
+export function getCosignPsbt(txid: string, vout: number, satoshis: bigint, vault_pubkey_hex: string, vault_claim_pubkey_hex: string, owner_pubkey_hex: string, vault_claim_height: bigint, open_claim_height: bigint, created_at_height: bigint, bitcoin_network: BitcoinNetwork, to_script_pubkey_hex: string, bitcoin_network_fee: bigint): string;
 
-export function signPsbt(
-  psbtHex: string,
-  bitcoinNetwork: BitcoinNetwork,
-  privateKeyHex: string,
-  finalize: boolean,
-): string;
+export function signPsbt(psbt_hex: string, bitcoin_network: BitcoinNetwork, private_key_hex: string, finalize: boolean): string;
 
-export function getCosignPsbt(
-  txid: string,
-  vout: number,
-  satoshis: bigint | number,
-  vaultPubkeyHex: string,
-  vaultClaimPubkeyHex: string,
-  ownerPubkeyHex: string,
-  vaultClaimHeight: bigint | number,
-  openClaimHeight: bigint | number,
-  createdAtHeight: bigint | number,
-  bitcoinNetwork: BitcoinNetwork,
-  toScriptPubkeyHex: string,
-  bitcoinNetworkFee: bigint | number,
-): string;
+export function signPsbtDerived(psbt_hex: string, xpriv_b58: string, xpriv_hd_path: string, finalize: boolean): string;
