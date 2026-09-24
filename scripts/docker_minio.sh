0cecf4eb86f82e62c46f2d2b272f4fbb9ab228e6
Blake Byrnes <blakebyrnes@gmail.com>
2026-09-24 12:27:12 -0400

    fix: restore MinIO for dev Docker and tests

--- scripts/docker_minio.sh
+++ scripts/docker_minio.sh
@@ -6,4 +6,4 @@ if [ "$(docker ps -q -f name=minio)" ]; then
     exit 0
 fi
 
-docker run --name minio --rm -p 9000:9000 -p 9001:9001 --mount type=tmpfs,destination=/data  quay.io/minio/minio:RELEASE.2025-09-07T16-13-09Z@sha256:14cea493d9a34af32f524e538b8346cf79f3321eff8e708c1e2960462bd8936e server /data --console-address ":9001" > /dev/null 2>&1 &
+docker run --name minio --rm -p 9000:9000 -p 9001:9001 --mount type=tmpfs,destination=/data  cgr.dev/chainguard/minio@sha256:bd014394a80898e68c149f2311fdf8d5a2c2f3bb2c33b9327ae6d02b4b065ae1 server /data --console-address ":9001" > /dev/null 2>&1 &
